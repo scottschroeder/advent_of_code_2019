745f7f4d893ac11802c9ca2e4d92d50b149efcbc
Scott Schroeder <scottschroeder@sent.com>
2019-12-29 17:25:22 -0500

    day9 part1/part2

--- src/challenges/day9.rs
+++ src/challenges/day9.rs
@@ -1,13 +1,17 @@
 use crate::util::parse_intcode;
+use crate::intcode::run_intcode;
 use anyhow::Result;
 
 pub fn day9_part1(input: &str) -> Result<String> {
     let intcode = parse_intcode(input)?;
-    Ok(format!("{}", 0))
+    let (_, out) = run_intcode(intcode, vec![1])?;
+    Ok(format!("{}", out[0]))
 }
 
 pub fn day9_part2(input: &str) -> Result<String> {
-    Ok(format!("{}", 0))
+    let intcode = parse_intcode(input)?;
+    let (_, out) = run_intcode(intcode, vec![2])?;
+    Ok(format!("{}", out[0]))
 }
 
 #[cfg(test)]
@@ -17,11 +21,11 @@ mod test {
 
     #[test]
     fn day9part1() {
-        assert_eq!(day9_part1(DAY9_INPUT).unwrap().as_str(), "0")
+        assert_eq!(day9_part1(DAY9_INPUT).unwrap().as_str(), "2752191671")
     }
 
     #[test]
     fn day9part2() {
-        assert_eq!(day9_part2(DAY9_INPUT).unwrap().as_str(), "0")
+        assert_eq!(day9_part2(DAY9_INPUT).unwrap().as_str(), "87571")
     }
 }
