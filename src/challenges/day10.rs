758bc11fba15a614015f07543ebaf2d001578144
Scott Schroeder <scottschroeder@sent.com>
2020-01-01 11:59:32 -0500

    day 10 + part2 complete

--- src/challenges/day10.rs
+++ src/challenges/day10.rs
@@ -1,54 +1,60 @@
-use self::space_map::{Map, Point};
+use self::space_map::{parse_map, Point, RadialMap};
 use anyhow::Result;
+use std::collections::HashSet;
 
 pub fn part1(input: &str) -> Result<String> {
-    let map = Map::from(input);
-    let max_point = find_max_asteroid(&map).expect("map does not have two points");
-    Ok(format!("{}", max_point.1))
+    let map = parse_map(input);
+    let rmap = find_max_asteroid(&map).expect("map does not have two points");
+    Ok(format!("{}", rmap.visable()))
 }
 
 pub fn part2(input: &str) -> Result<String> {
-    Ok(format!("{}", 0))
+    let map = parse_map(input);
+    let rmap = find_max_asteroid(&map).expect("map does not have two points");
+    let p = rmap.sweep().nth(200 - 1).unwrap();
+    let answer = p.x * 100 + p.y;
+    Ok(format!("{}", answer))
 }
 
-fn find_max_asteroid(map: &Map) -> Option<(Point, usize)> {
-    let mut max_point: Option<(Point, usize)> = None;
-    for p in &map.data {
-        let vis = map.visible_from(*p);
-        let len = vis.len();
-        if len > max_point.unwrap_or((Point::new(-1, -1), 0)).1 {
-            max_point = Some((*p, len))
+fn find_max_asteroid(map: &HashSet<Point>) -> Option<RadialMap> {
+    let mut max_point: Option<(RadialMap, usize)> = None;
+    for p in map {
+        let rmap = RadialMap::new(*p, map);
+        let len = rmap.visable();
+        let prev = if let Some((_, old_len)) = max_point {
+            old_len
+        } else {
+            0
+        };
+        if len > prev {
+            max_point = Some((rmap, len))
         }
     }
-    max_point
+    max_point.map(|(r, _)| r)
 }
 
 mod space_map {
-    pub use self::point::{LinePoints, Point};
-    use std::collections::HashSet;
+    pub use self::point::{Point, SortedPointArray};
+    use std::cmp::Ordering;
+    use std::collections::{BTreeMap, HashSet};
+    use std::f64::consts::PI;
 
-    fn parse_map(input: &str) -> Map {
-        let mut max_w = 0usize;
-        let mut line = 0usize;
+    pub fn parse_map(input: &str) -> HashSet<Point> {
         let mut points = HashSet::new();
-        for row in input.lines() {
+        for (line, row) in input.lines().enumerate() {
             for (cidx, c) in row.chars().enumerate() {
                 if c == '#' {
                     points.insert(Point::new(cidx as i64, line as i64));
                 }
-                max_w = std::cmp::max(max_w, cidx);
             }
-            line += 1;
         }
-        Map {
-            data: points,
-            width: max_w as i64 + 1,
-            height: line as i64,
-        }
+        points
     }
 
     mod point {
         use num::Integer;
+        use std::cmp::{Ordering, Reverse};
+        use std::collections::BinaryHeap;
         use std::fmt;
         use std::ops::{Add, Mul, Sub};
         #[derive(Clone, Copy, PartialEq, Eq, Hash)]
@@ -60,6 +66,18 @@ mod space_map {
         impl Point {
             pub fn new(x: i64, y: i64) -> Self {
                 Self { x, y }
+            }
+
+            pub fn ray(self, other: Point) -> Point {
+                if self == other {
+                    return Point::new(0, 0);
+                }
+                let diff = other - self;
+                let gcd = diff.x.gcd(&diff.y);
+                Point {
+                    x: diff.x / gcd,
+                    y: diff.y / gcd,
+                }
             }
         }
 
@@ -98,38 +116,98 @@ mod space_map {
             }
         }
 
-        pub struct LinePoints {
-            base: Point,
-            ray: Point,
-            n: i64,
+        pub struct SortedPointArray {
+            origin: Point,
+            heap: BinaryHeap<Reverse<PointVec>>,
         }
 
-        impl LinePoints {
-            pub fn new(src: Point, dst: Point) -> Self {
-                assert!(src != dst);
-                let diff = dst - src;
-                let gcd = diff.x.gcd(&diff.y);
-                let ray = Point::new(diff.x / gcd, diff.y / gcd);
-
-                Self {
-                    base: dst,
-                    ray,
-                    n: 0,
+        impl SortedPointArray {
+            pub fn new(origin: Point) -> SortedPointArray {
+                SortedPointArray {
+                    origin,
+                    heap: BinaryHeap::new(),
                 }
+            }
+            pub fn insert(&mut self, point: Point) {
+                let v = PointVec {
+                    src: self.origin,
+                    dst: point,
+                };
+                self.heap.push(Reverse(v));
+            }
+
+            pub fn into_vec(mut self) -> Vec<Point> {
+                let mut out = Vec::with_capacity(self.heap.len());
+                while let Some(v) = self.heap.pop() {
+                    out.push(v.0.dst)
+                }
+                out
             }
         }
 
-        impl Iterator for LinePoints {
-            type Item = Point;
-            fn next(&mut self) -> Option<Self::Item> {
-                self.n += 1;
-                Some(self.base + self.ray * self.n)
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        struct PointVec {
+            src: Point,
+            dst: Point,
+        }
+
+        impl Ord for PointVec {
+            fn cmp(&self, other: &Self) -> Ordering {
+                self.partial_cmp(other).unwrap()
+            }
+        }
+
+        impl PartialOrd for PointVec {
+            fn partial_cmp(&self, other: &PointVec) -> Option<Ordering> {
+                fn magnitude_squared(p: Point) -> i64 {
+                    p.x * p.x + p.y * p.y
+                }
+                magnitude_squared(self.dst - self.src)
+                    .partial_cmp(&magnitude_squared(other.dst - other.src))
             }
         }
 
         #[cfg(test)]
         mod test {
             use super::*;
+
+            fn check_sort_points<F: Fn(Point, &[Point]) -> Vec<Point>>(f: F) {
+                let src = Point::new(11, 13);
+                let points = vec![
+                    Point::new(11, 12),
+                    Point::new(11, 2),
+                    Point::new(11, 3),
+                    Point::new(11, 5),
+                    Point::new(11, 1),
+                ];
+
+                let s_pts = f(src, &points);
+                let yvals = s_pts.into_iter().map(|v| v.y).collect::<Vec<_>>();
+                assert_eq!(yvals, vec![12, 5, 3, 2, 1]);
+            }
+
+            #[test]
+            fn pointvec_order() {
+                check_sort_points(|src: Point, points: &[Point]| {
+                    let mut pv = points
+                        .iter()
+                        .map(|dst| PointVec { src, dst: *dst })
+                        .collect::<Vec<_>>();
+                    pv.sort();
+                    pv.into_iter().map(|v| v.dst).collect::<Vec<_>>()
+                });
+            }
+
+            #[test]
+            fn sorted_point_array_order() {
+                check_sort_points(|src: Point, points: &[Point]| {
+                    let mut spa = SortedPointArray::new(src);
+                    for p in points {
+                        spa.insert(*p);
+                    }
+                    spa.into_vec()
+                });
+            }
 
             #[test]
             fn point_add() {
@@ -153,83 +231,118 @@ mod space_map {
                 let p2 = p1 * 3;
                 assert_eq!(p2, Point::new(9, 12));
             }
-
-            #[test]
-            fn point_iter() {
-                let origin = Point::new(3, 4);
-                let target = Point::new(4, 6);
-                let points = LinePoints::new(origin, target).take(5).collect::<Vec<_>>();
-                assert_eq!(
-                    points,
-                    vec![
-                        Point::new(5, 8),
-                        Point::new(6, 10),
-                        Point::new(7, 12),
-                        Point::new(8, 14),
-                        Point::new(9, 16),
-                    ]
-                )
-            }
-
-            #[test]
-            fn point_iter_reduce() {
-                let origin = Point::new(3, 4);
-                let target = Point::new(5, 6); // 2x2 should be reduced to 1x1
-                let points = LinePoints::new(origin, target).take(5).collect::<Vec<_>>();
-                assert_eq!(
-                    points,
-                    vec![
-                        Point::new(6, 7),
-                        Point::new(7, 8),
-                        Point::new(8, 9),
-                        Point::new(9, 10),
-                        Point::new(10, 11),
-                    ]
-                )
-            }
         }
     }
 
-    #[derive(Debug, Clone)]
-    pub struct Map {
-        pub data: HashSet<Point>,
-        width: i64,
-        height: i64,
+    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+    struct RadiallySortedPoint(Point);
+
+    impl RadiallySortedPoint {
+        /// This considers zero at the 12 o'clock position
+        /// and clockwise rotation is the positive direction.
+        pub fn unit_circle(self) -> f64 {
+            let x = self.0.x as f64;
+            let y = self.0.y as f64;
+            let t = y.atan2(x);
+            ((t + PI / 2.0) + 2.0 * PI).rem_euclid(2.0 * PI)
+        }
     }
 
-    impl Map {
-        #[inline]
-        fn in_map_bounds(&self, p: Point) -> bool {
-            trace!(slog_scope::logger(), "{:?}", p);
-            (p.x >= 0 && p.x < self.width) && (p.y >= 0 && p.y < self.height)
+    impl Ord for RadiallySortedPoint {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.partial_cmp(other).unwrap()
         }
+    }
 
-        #[inline]
-        fn is_object(&self, p: Point) -> bool {
-            self.data.contains(&p)
+    impl PartialOrd for RadiallySortedPoint {
+        fn partial_cmp(&self, other: &RadiallySortedPoint) -> Option<Ordering> {
+            self.unit_circle().partial_cmp(&other.unit_circle())
         }
+    }
 
-        pub fn visible_from(&self, o: Point) -> HashSet<Point> {
-            let mut hidden = HashSet::new();
-            hidden.insert(o);
+    #[derive(Debug)]
+    pub struct RadialMap {
+        pub origin: Point,
+        points: Vec<Vec<Point>>,
+    }
 
-            for p in &self.data {
-                if o == *p || hidden.contains(p) {
+    impl RadialMap {
+        pub fn new(origin: Point, points: &HashSet<Point>) -> RadialMap {
+            let mut builder = BTreeMap::new();
+            let mut flat_map = Vec::new();
+
+            for p in points {
+                if *p == origin {
                     continue;
                 }
-                hidden.extend(
-                    LinePoints::new(o, *p)
-                        .take_while(|t| self.in_map_bounds(*t))
-                        .filter(|t| self.is_object(*t)),
-                )
+                let ray = RadiallySortedPoint(origin.ray(*p));
+                let heap = builder
+                    .entry(ray)
+                    .or_insert_with(|| SortedPointArray::new(origin));
+                heap.insert(*p);
             }
-            self.data.difference(&hidden).cloned().collect()
+            for (_, angle_points) in builder {
+                flat_map.push(angle_points.into_vec())
+            }
+            RadialMap {
+                origin,
+                points: flat_map,
+            }
+        }
+        pub fn visable(&self) -> usize {
+            self.points.len()
+        }
+
+        pub fn sweep(&self) -> RadialSweep<'_> {
+            RadialSweep::new(self)
         }
     }
 
-    impl<T: AsRef<str>> From<T> for Map {
-        fn from(input: T) -> Self {
-            parse_map(input.as_ref())
+    pub struct RadialSweep<'a> {
+        map: &'a RadialMap,
+        theta: usize,
+        radius: usize,
+        depth: usize,
+    }
+
+    impl<'a> RadialSweep<'a> {
+        fn new(map: &'a RadialMap) -> RadialSweep<'a> {
+            let depth = map.points.iter().map(|c| c.len()).max().unwrap();
+            RadialSweep {
+                map,
+                theta: 0,
+                radius: 0,
+                depth,
+            }
+        }
+        fn get(&self) -> Option<Point> {
+            let row = &self.map.points[self.theta];
+            if self.radius < row.len() {
+                Some(row[self.radius])
+            } else {
+                None
+            }
+        }
+        fn increment(&mut self) {
+            self.theta += 1;
+            if self.theta == self.map.points.len() {
+                self.theta = 0;
+                self.radius += 1;
+            }
+        }
+    }
+
+    impl<'a> Iterator for RadialSweep<'a> {
+        type Item = Point;
+        fn next(&mut self) -> Option<Self::Item> {
+            while self.radius < self.depth {
+                let p = self.get();
+                self.increment();
+                if let Some(p) = p {
+                    return Some(p);
+                }
+            }
+            None
         }
     }
 }
@@ -238,12 +351,36 @@ mod space_map {
 mod test {
     use super::*;
     use crate::challenges::test::*;
+    use std::collections::HashMap;
+
+    const ADVENT_LARGE_EXAMPLE: &str = "\
+        .#..##.###...#######\n\
+        ##.############..##.\n\
+        .#.######.########.#\n\
+        .###.#######.####.#.\n\
+        #####.##.#.##.###.##\n\
+        ..#####..#.#########\n\
+        ####################\n\
+        #.####....###.#.#.##\n\
+        ##.#################\n\
+        #####.##.###..####..\n\
+        ..######..##.#######\n\
+        ####.##.####...##..#\n\
+        .#####..#.######.###\n\
+        ##...#.##########...\n\
+        #.##########.#######\n\
+        .####.#.###.###.#.##\n\
+        ....##.##.###..#####\n\
+        .#.#.###########.###\n\
+        #.#.#.#####.####.###\n\
+        ###.##.####.##.#..##
+    ";
 
     fn check_map_input(input: &str, p: Point, len: usize) {
-        let map = Map::from(input);
-        let max_point = find_max_asteroid(&map).expect("map does not have two points");
-        assert_eq!(max_point.0, p);
-        assert_eq!(max_point.1, len);
+        let map = parse_map(input);
+        let rmap = find_max_asteroid(&map).expect("map does not have two points");
+        assert_eq!(rmap.origin, p);
+        assert_eq!(rmap.visable(), len);
     }
 
     #[test]
@@ -310,29 +447,35 @@ mod test {
 
     #[test]
     fn advent_example_5() {
-        let input = "\
-            .#..##.###...#######\n\
-            ##.############..##.\n\
-            .#.######.########.#\n\
-            .###.#######.####.#.\n\
-            #####.##.#.##.###.##\n\
-            ..#####..#.#########\n\
-            ####################\n\
-            #.####....###.#.#.##\n\
-            ##.#################\n\
-            #####.##.###..####..\n\
-            ..######..##.#######\n\
-            ####.##.####...##..#\n\
-            .#####..#.######.###\n\
-            ##...#.##########...\n\
-            #.##########.#######\n\
-            .####.#.###.###.#.##\n\
-            ....##.##.###..#####\n\
-            .#.#.###########.###\n\
-            #.#.#.#####.####.###\n\
-            ###.##.####.##.#..##
-        ";
+        let input = ADVENT_LARGE_EXAMPLE;
         check_map_input(input, Point::new(11, 13), 210);
+    }
+
+    #[test]
+    fn advent_p2_asteroid_sweep() {
+        let input = ADVENT_LARGE_EXAMPLE;
+        let mut checkpoints = HashMap::new();
+        checkpoints.insert(1, Point::new(11, 12));
+        checkpoints.insert(2, Point::new(12, 1));
+        checkpoints.insert(3, Point::new(12, 2));
+        checkpoints.insert(10, Point::new(12, 8));
+        checkpoints.insert(20, Point::new(16, 0));
+        checkpoints.insert(50, Point::new(16, 9));
+        checkpoints.insert(100, Point::new(10, 16));
+        checkpoints.insert(199, Point::new(9, 6));
+        checkpoints.insert(200, Point::new(8, 2));
+        checkpoints.insert(201, Point::new(10, 9));
+        checkpoints.insert(299, Point::new(11, 1));
+
+        let map = parse_map(input);
+        let rmap = find_max_asteroid(&map).expect("map does not have two points");
+        assert_eq!(rmap.origin, Point::new(11, 13));
+        for (idx, a) in rmap.sweep().enumerate() {
+            let count = idx + 1;
+            if let Some(e) = checkpoints.get(&count) {
+                assert_eq!(a, *e, "idx: {}", idx)
+            }
+        }
     }
 
     #[test]
@@ -342,6 +485,6 @@ mod test {
 
     #[test]
     fn check_part2() {
-        assert_eq!(part2(DAY10_INPUT).unwrap().as_str(), "0")
+        assert_eq!(part2(DAY10_INPUT).unwrap().as_str(), "1309")
     }
 }
